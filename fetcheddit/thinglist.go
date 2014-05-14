4b12b67696d5694d2b8ebaec97e566f7fbfbefa8
Lee Clagett <code@leeclagett.com>
2014-05-14 08:22:42 -0400

    Finished comment thingList

--- fetcheddit/thinglist.go
+++ fetcheddit/thinglist.go
@@ -5,19 +5,35 @@ import (
 	"errors"
 )
 
+// Aggregate returned in the channel that fetches reddit thing lists
 type nextChunk struct {
-	nextThings     []grokeddit.Thing
-	moreThings     bool
-	retrievalError error
+	nextThings     []grokeddit.Thing // The list of things retrieved
+	moreThings     bool              // True if there is another list of things
+	retrievalError error             // Non-nil if there was an error trying to retrieve the list of things
 }
 
+/* An abstraction for a list of reddit things. The paginated lists are
+"flattened" to appear as giant single list. The pages are retreived
+asynchronously, so there is less blocking time waiting for the next page. */
 type thingList struct {
-	fetchMoreThings func() ([]grokeddit.Thing, bool, error)
-	currentThings   thingIterater // Dictates how a slice returned by fetchMoreThings is iterated
-	moreThings      bool          // The last value returned by fetchMoreThings
-	chunkChannel    chan nextChunk
+	fetchMoreThings func() ([]grokeddit.Thing, bool, error) // Function that can be called in a separate goroutine to retrieve the next page of things
+	currentThings   thingIterater                           // Dictates how a slice returned by fetchMoreThings is iterated
+	moreThings      bool                                    // The last value returned by fetchMoreThings
+	chunkChannel    chan nextChunk                          // Receives the next page of reddit things
 }
 
+/* Creates a new thing list. 
+
+The closure arugment (fetchedGroked) allows the caller to handle differences in
+the reddit API. The lists can be sent in two different ways depending on the 
+context, and the caller must massage those differences into a single Groked 
+object.
+
+The contentFetcher allows the caller to modify how the reddit things are being 
+retrieved, mainly used for testing (possibly different URLs too).
+
+The anchor specifies a starting point, and an iteration direction. NULL 
+indicates to start from the beginning, and iterate forward. */
 func fetchThingList(
 	fetchGroked func(Fetcher, *AnchorPoint) (grokeddit.Groked, error),
 	contentFetcher Fetcher,
@@ -90,6 +106,7 @@ func fetchThingList(
 	return newThingList, nil
 }
 
+// Asynchronously fetches the next page of reddit things.
 func (list *thingList) fetchNextBlockAsync() {
 	go func() {
 		nextThings, moreThings, error := list.fetchMoreThings()
@@ -97,10 +114,12 @@ func (list *thingList) fetchNextBlockAsync() {
 	}()
 }
 
+// Return true if the thingList has another thing available.
 func (list *thingList) hasNext() bool {
 	return list.moreThings || list.currentThings.hasNext()
 }
 
+// Return the next thing in the list, or error.
 func (list *thingList) getNext() (grokeddit.Thing, error) {
 	if !list.hasNext() {
 		return grokeddit.Thing{}, errors.New("No more things to iterate")

9bfd1a9ddcb080c36ddbfd1b650afcfbb3c902e1
Lee Clagett <code@leeclagett.com>
2014-06-22 12:08:43 -0400

    Changing order of reverse, and improving channel usage in test
    So the effective go tutorial failed to mention that there was an
    optional return value from a channel receive operation. That would've
    been nice to know. Somehow a Go dev didn't mention it either in a
    mailing list, maybe this is new or something ...

--- fetcheddit/fetcher_test.go
+++ fetcheddit/fetcher_test.go
@@ -124,6 +124,50 @@ const (
                     "banned_by": null,
                     "media_embed": {},
                     "subreddit": "redditdev",
+                    "selftext_html": "<html>2</html>",
+                    "likes": null,
+                    "secure_media": null,
+                    "link_flair_text": null,
+                    "id": "20e6ir",
+                    "gilded": 0,
+                    "secure_media_embed": {},
+                    "clicked": false,
+                    "stickied": false,
+                    "author": "Grimzentide",
+                    "media": null,
+                    "score": 1,
+                    "approved_by": null,
+                    "over_18": false,
+                    "hidden": false,
+                    "thumbnail": "",
+                    "subreddit_id": "t5_2qizd",
+                    "edited": false,
+                    "link_flair_css_class": null,
+                    "author_flair_css_class": null,
+                    "downs": 1,
+                    "saved": false,
+                    "is_self": true,
+                    "permalink": "/r/redditdev/comments/20e6ir/is_this_the_most_efficient_way_to_run_this_code/",
+                    "name": "t3_20e6ir",
+                    "created": 1394822733,
+                    "url": "http://www.reddit.com/r/redditdev/comments/20e6ir/is_this_the_most_efficient_way_to_run_this_code/",
+                    "author_flair_text": null,
+                    "title": "Is this the most efficient way to run this code?",
+                    "created_utc": 1394793933,
+                    "ups": 2,
+                    "num_comments": 3,
+                    "visited": false,
+                    "num_reports": null,
+                    "distinguished": null
+                }
+            },
+            {
+                "kind": "t3",
+                "data": {
+                    "domain": "self.redditdev",
+                    "banned_by": null,
+                    "media_embed": {},
+                    "subreddit": "redditdev",
                     "selftext_html": "<html>1</html>",
                     "selftext": "1",
                     "likes": null,
@@ -157,50 +201,6 @@ const (
                     "created_utc": 1394831435,
                     "ups": 11,
                     "num_comments": 2,
-                    "visited": false,
-                    "num_reports": null,
-                    "distinguished": null
-                }
-            },
-            {
-                "kind": "t3",
-                "data": {
-                    "domain": "self.redditdev",
-                    "banned_by": null,
-                    "media_embed": {},
-                    "subreddit": "redditdev",
-                    "selftext_html": "<html>2</html>",
-                    "likes": null,
-                    "secure_media": null,
-                    "link_flair_text": null,
-                    "id": "20e6ir",
-                    "gilded": 0,
-                    "secure_media_embed": {},
-                    "clicked": false,
-                    "stickied": false,
-                    "author": "Grimzentide",
-                    "media": null,
-                    "score": 1,
-                    "approved_by": null,
-                    "over_18": false,
-                    "hidden": false,
-                    "thumbnail": "",
-                    "subreddit_id": "t5_2qizd",
-                    "edited": false,
-                    "link_flair_css_class": null,
-                    "author_flair_css_class": null,
-                    "downs": 1,
-                    "saved": false,
-                    "is_self": true,
-                    "permalink": "/r/redditdev/comments/20e6ir/is_this_the_most_efficient_way_to_run_this_code/",
-                    "name": "t3_20e6ir",
-                    "created": 1394822733,
-                    "url": "http://www.reddit.com/r/redditdev/comments/20e6ir/is_this_the_most_efficient_way_to_run_this_code/",
-                    "author_flair_text": null,
-                    "title": "Is this the most efficient way to run this code?",
-                    "created_utc": 1394793933,
-                    "ups": 2,
-                    "num_comments": 3,
                     "visited": false,
                     "num_reports": null,
                     "distinguished": null
@@ -285,39 +285,42 @@ var (
 )
 
 type TestFetch struct {
-	nextValue     chan *string
-	lastFetchPath chan *string
+	nextValue     chan string
+	lastFetchPath chan string
 }
 
-func CreateTestFetch(values []string) TestFetch {
+
+
+func CreateTestFetch(values []string) *TestFetch {
 	newTestFetch := TestFetch{
-		make(chan *string, len(values)),
-		make(chan *string, len(values))}
+		make(chan string, len(values)),
+		make(chan string, len(values)),
+	}
 
 	defer close(newTestFetch.nextValue)
 	for _, value := range values {
-		newTestFetch.nextValue <- &value
+		newTestFetch.nextValue <- value
 	}
 
-	return newTestFetch
+	return &newTestFetch
 }
 
-func (testFetch TestFetch) Fetch(path string) (io.ReadCloser, error) {
-	nextValue := <-testFetch.nextValue
-	if nextValue == nil {
+func (testFetch *TestFetch) Fetch(path string) (io.ReadCloser, error) {
+	nextValue, ok := <-testFetch.nextValue
+	if !ok {
 		close(testFetch.lastFetchPath)
 		return nil, errors.New("No more values to fetch")
 	}
 
-	testFetch.lastFetchPath <- &path
-	return ioutil.NopCloser(strings.NewReader(*nextValue)), nil
+	testFetch.lastFetchPath <- path
+	return ioutil.NopCloser(strings.NewReader(nextValue)), nil
 }
 
-func (testFetch TestFetch) GetNextFetchLocation() (string, error) {
-	nextValue := <-testFetch.lastFetchPath
-	if nextValue == nil {
+func (testFetch *TestFetch) GetNextFetchLocation() (string, error) {
+	nextValue, ok := <-testFetch.lastFetchPath
+	if !ok {
 		return "", errors.New("No more fetch locations")
 	}
 
-	return *nextValue, nil
+	return nextValue, nil
 }
